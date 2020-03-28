c9f3cc2850436c0cfd58ebb48c67c91ff323efbc
Marcin Dubel <marcin@appsilon.com>
2020-03-28 14:11:37 +0100

    Connection to mapbox gl

--- analysis.R
+++ analysis.R
@@ -2,6 +2,11 @@ library(arrow)
 library(purrr)
 library(lubridate)
 library(dplyr)
+library(geohashTools)
+library(leaflet)
+library(sp)
+#remotes::install_github("rstudio/leaflet.mapboxgl")
+library(leaflet.mapboxgl)
 
 read_full_data <- function() {
   dataDir <- "veraset"
@@ -20,7 +25,22 @@ as.Date(as.POSIXct(value, origin="1970-01-01"))
 dates <- map(data, ~ unique(date(as.Date(as.POSIXct(as.numeric(.$utc_timestamp), origin="1970-01-01")))))
 
 single_person <- function(data, id) {
-  lapply(data, function(set) {set %>% dplyr::filter(caid == id)}) %>% dplyr::bind_rows()
+  data %>% dplyr::filter(caid == id)
 }
 
-single_data <- single_person(data, uid[1])
+data_decoded <- function(dataset) {
+  geo_decode <- geohashTools::gh_decode(dataset$geo_hash)
+  dataset$latitude <- geo_decode$latitude
+  dataset$longitude <- geo_decode$longitude
+  dataset$utc_timestamp <- as.POSIXct(as.numeric(dataset$utc_timestamp), origin="1970-01-01")
+  coordinates(dataset) <- ~longitude+latitude
+  dataset$geo_hash <- NULL
+  dataset
+}
+
+single_data <- single_person(data, uid[500]) %>% data_decoded()
+#data_decoded <- data %>% data_decoded()
+
+options(mapbox.accessToken = "pk.eyJ1IjoibWR1YmVsIiwiYSI6ImNrNTgweTlwOTAweDczbXBneTJtNTA2Y2UifQ.kIHidFuI7ooK0KU5yigvqg")
+
+leaflet(single_data) %>% addMapboxGL(style = "mapbox://styles/mdubel/ck8bl3juq0zvd1iq3h9f1xo70") %>% addMarkers() %>% addTiles()
