a5c8e8622941fbbd629c6f70b10cbed7765941ea
Filip Stachura <filip@appsilondatascience.com>
2020-03-29 19:38:36 +0200

    Check risk button.

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,5 +1,6 @@
 ui <- fluidPage(
   tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")),
   dateInput("date", label = NULL, value = "2020-03-26"),
-  leafletOutput("risk_map", width = '100%', height = '100vh')
+  leafletOutput("risk_map", width = '100%', height = '100vh'),
+  actionButton("showPopup", "Check your risk")
 )
