6949401dd40083e5bd43de3fbfd3f66f38204048
Marcin Dubel <marcin@appsilon.com>
2020-03-29 23:09:19 +0200

    Modified colors for gauge

--- shiny/server.R
+++ shiny/server.R
@@ -72,7 +72,8 @@ server <- function(input, output, session) {
           max = 1, 
           sectors = gaugeSectors(success = c(0.0, 0.2), 
                                  warning = c(0.21, 0.7),
-                                 danger = c(0.71, 0.9)))
+                                 danger = c(0.71, 0.9),
+                                 colors = c('rgba(54, 92, 134, 0.1)', 'rgba(54, 92, 134, 0.5)', 'rgba(54, 92, 134, 1)')))
   })
   
   riskProfiles <- list(
