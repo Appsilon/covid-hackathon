e25972bf038090fab5fea2e8307dc6347283b943
Marcin Dubel <marcin@appsilon.com>
2020-03-29 14:39:16 +0200

    Map with calendat feature

--- shiny/server.R
+++ shiny/server.R
@@ -1,10 +1,24 @@
 server <- function(input, output, session) {
   #pageranks <- list( "12-01" = pagerank_for_dataset("12-01") )
   #risky_loc <- list( "12-01" = get_risky_locations(pageranks$`12-01`) )
+  data_selected <- reactive({
+    if(input$date == "2020-03-26")
+      list(lng = 174.768, lat = -36.852)
+    else if(input$date == "2020-03-25")
+      list(lng = 173.768, lat = -37.852)
+  })
+  
   output$risk_map <- renderLeaflet({
     #plot_risky_locations(risky_loc$`12-01`)
     leaflet() %>%
-      addTiles() %>%  # Add default OpenStreetMap map tiles
-      addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
+      addTiles() %>% 
+      addCircles(lng = 174.768, lat = -36.852, radius = 3900) %>% 
+      setView(lng = 174.768, lat = -36.852, zoom = 8)
   }) 
+  
+  observe({
+    leafletProxy("risk_map") %>% 
+      clearShapes() %>% 
+      addCircles(lng = data_selected()$lng, lat = data_selected()$lat, radius = 3900)
+  })
 }
