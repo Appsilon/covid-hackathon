3968842f674916bf57ff40b623f46b5b5e19ff8d
Filip Stachura <filip@appsilondatascience.com>
2020-03-29 20:08:24 +0200

    Small UI changes.

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,5 +1,9 @@
-ui <- fluidPage(
-  tags$head(tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")),
+ui <- fluidPage(title = "Community Shield",
+  tags$head(
+    tags$link(rel = "manifest", href = "manifest.json"),
+    tags$link(rel="icon", type="image/png", href="32.png"),
+    tags$link(rel = "stylesheet", type = "text/css", href = "css/style.css")
+  ),
   dateInput("date", label = NULL, value = "2020-03-26"),
   leafletOutput("risk_map", width = '100%', height = '100vh'),
   actionButton("showPopup", "Check your risk")
